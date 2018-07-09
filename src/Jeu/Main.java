feb5c1ab0f82a06cf8cf45094a20148e945cae52
mercure84 <julien.marcesse@gmail.com>
2018-07-09 09:29:27 +0200

    refactoring pour que ça marche sur pc

---
+++ src/Jeu/Main.java
@@ -0,0 +1,34 @@
+package Jeu;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * classe qui renvoie différentes méthodes menu pour choisir le jeu ou rejouer
+ * méthode main qui constitue le point d'entrée du programme.
+ * 
+ * @author julie
+ *
+ */
+public class Main {
+
+	// initiatilisation du logger
+	static final Logger logger = LogManager.getLogger();
+
+	public static void main(String[] args) {
+// affichage du menu pour choisir le jeu et le mode via le constructeur de la classe partie + définitions de certains paramètres de la partie
+		
+		while(true) {
+		
+		
+		Partie partie = new Partie();
+		
+		partie.start();
+		
+		partie = null;
+		
+
+
+		}
+		
+	}
+}
